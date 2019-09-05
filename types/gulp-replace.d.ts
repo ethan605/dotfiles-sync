0f88d522e2596cbdd2650e819b773eb71a11356b
Ethan Nguyen <thanhnx.605@gmail.com>
2019-09-05 16:04:45 +0700

    Add typings for gulp-replace & gulp-run

---
+++ types/gulp-replace.d.ts
@@ -0,0 +1,15 @@
+declare module 'gulp-replace' {
+  import { Transform } from 'stream';
+
+  interface Options {
+    skipBinary?: boolean;
+  }
+
+  type ReplaceFunction = (match: string, p1: string, offset: number, string: string) => string;
+
+  export default function replace(
+    search: RegExp | string,
+    replace: ReplaceFunction | string,
+    options?: Options
+  ): Transform;
+}
