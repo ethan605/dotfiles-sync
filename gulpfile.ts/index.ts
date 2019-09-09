f91fc5da7219af7b7d78686e8b3c03132a4b4c04
Ethan Nguyen <thanhnx.605@gmail.com>
2019-09-09 19:57:31 +0700

    Finalize definitions for gulp-prompt

--- gulpfile.ts/index.ts
+++ gulpfile.ts/index.ts
@@ -1,1 +1,44 @@
 import './backup';
+
+import gulp from 'gulp';
+import { confirm, Question, ChainResponse } from 'gulp-prompt';
+
+let counter = 0;
+
+const chainFunction = (options: Question, resp: ChainResponse): Question => {
+  console.log('Here is the selection', options, resp);
+
+  if (counter === 0) {
+    counter += 1;
+
+    return {
+      ...options,
+      message: 'Second interation, please enter selection?',
+    };
+  }
+
+  return undefined;
+};
+
+/**
+ * The following is a sample gulp file for chaining requests.  It
+ * will pass a chaining function to the confirm function and will
+ * allow the user to chain multiple requests together
+ * variables
+ *
+ */
+gulp.task('chainConfirm', cb => {
+  gulp.src('package.json', { allowEmpty: true }).pipe(
+    confirm(
+      {
+        type: 'input',
+        name: 'env',
+        message: 'Hello First interation, please enter selection?',
+        chainFunction: chainFunction,
+      },
+      res => console.log('Result', res)
+    )
+  );
+
+  cb();
+});
--- types/gulp-prompt/index.d.ts
+++ types/gulp-prompt/index.d.ts
@@ -1,26 +1,29 @@
 declare module 'gulp-prompt' {
   import { Transform } from 'stream';
 
-  interface ConfirmOption {
-    message: string;
-    default?: boolean;
+  export interface ChainResponse {
+    val: boolean;
   }
+
+  export type CallbackHandler = ({ [key in string]: string }) => void;
 
   export interface Question {
     choices?: number[] | string[] | object[] | Function;
     default?: string | number | boolean | Array | Function;
     filter?: Function;
-    message?: string | Function;
+    message: string | Function;
     name?: string;
     pageSize?: number;
     prefix?: number;
     suffix?: number;
+    templateOptions?: { [key in string]: string };
     transformer?: Function;
     type?: 'input' | 'number' | 'confirm' | 'list' | 'rawlist' | 'expand' | 'checkbox' | 'password' | 'editor';
+    chainFunction?(options: Question, response: ChainResponse): Question | undefined;
     validate?(input: string, answers: object): boolean;
-    when?: (answers: object) => boolean | boolean;
+    when?(answers: object): boolean;
   }
 
-  export function confirm(options?: string | ConfirmOption): Transform;
-  export function prompt(questions: Question | Question[], callback: Function): Transform;
+  export function confirm(options?: string | Question, callback?: CallbackHandler): Transform;
+  export function prompt(questions: Question | Question[], callback: CallbackHandler): Transform;
 }
