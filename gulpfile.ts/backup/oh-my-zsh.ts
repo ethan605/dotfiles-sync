1e8bcd9de5a243c42aca08a33e0c1644368a6dcf
Ethan Nguyen <thanhnx.605@gmail.com>
2020-01-10 14:48:16 +0700

    Add PITOP_GITHUB_TOKEN as sensitive data to be removed during backup process

--- gulpfile.ts/backup/oh-my-zsh.ts
+++ gulpfile.ts/backup/oh-my-zsh.ts
@@ -20,6 +20,7 @@ const removeSensitiveData = (): NodeJS.ReadWriteStream =>
     .src(wrapHomeDir('.zshrc'))
     .pipe(replace(/HOMEBREW_GITHUB_API_TOKEN=".*"/, 'HOMEBREW_GITHUB_API_TOKEN=""'))
     .pipe(replace(/JEKYLL_GITHUB_TOKEN=".*"/, 'JEKYLL_GITHUB_TOKEN=""'))
+    .pipe(replace(/PITOP_GITHUB_TOKEN=".*"/, 'PITOP_GITHUB_TOKEN=""'))
     .pipe(replace(/GOOGLE_APPLICATION_CREDENTIALS=".*"/, 'GOOGLE_APPLICATION_CREDENTIALS=""'))
     .pipe(replace(/GOOGLE_PROJECT_ID=".*"/, 'GOOGLE_PROJECT_ID=""'))
     .pipe(gulp.dest(DEST_DIR));
