c81121536bb9aaf28188947089549398a7f3ccf0
Ethan <thanhnx.605@gmail.com>
2020-04-07 20:15:15 +0700

    :recycle: Refactor command reading tasks

--- gulpfile.ts/backup/brew.ts
+++ gulpfile.ts/backup/brew.ts
@@ -1,37 +1,14 @@
-import _ from 'lodash';
-import Undertaker from 'undertaker';
-
 // Gulp modules
 import gulp from 'gulp';
-import file from 'gulp-file';
 
 // Helpers
-import { readCommandOutputs } from '../helpers';
+import { composeReadCommandTask } from '../helpers';
+import type { ReadCommandTask } from '../helpers';
 
-// Constants
-import { BACKUP_DIR } from '../constants';
-
-interface Task {
-  command: string;
-  title: string;
-}
-
-const TASKS: Task[] = [
-  { command: 'brew list', title: 'formulaes' },
-  { command: 'brew cask list', title: 'casks' },
-  { command: 'brew tap', title: 'taps' },
+const TASKS: ReadCommandTask[] = [
+  { title: 'formulaes', command: 'brew list' },
+  { title: 'casks', command: 'brew cask list' },
+  { title: 'taps', command: 'brew tap' },
 ];
 
-const composeBackupTask = ({ command, title }: Task): Undertaker.Task => {
-  const output = readCommandOutputs(command).toString();
-  const data = _.compact(output.split('\n'));
-  const backupTask = (): NodeJS.ReadWriteStream =>
-    file(`${title}.json`, JSON.stringify(data), { src: true }).pipe(gulp.dest(`${BACKUP_DIR}/brew`));
-
-  // Rename sub tasks
-  Object.defineProperty(backupTask, 'name', { value: `brew${_.capitalize(title)}`, configurable: true });
-
-  return backupTask;
-};
-
-export default gulp.parallel(TASKS.map(composeBackupTask));
+export default gulp.parallel(TASKS.map(composeReadCommandTask('brew')));
--- gulpfile.ts/helpers.ts
+++ gulpfile.ts/helpers.ts
@@ -1,11 +1,36 @@
 import { execSync } from 'child_process';
+import _ from 'lodash';
+import Undertaker from 'undertaker';
 
-export const readCommandOutputs = (...commands: string[]): string[] =>
-  commands.map(command => execSync(command).toString());
+// Gulp modules
+import gulp from 'gulp';
+import file from 'gulp-file';
+
+import { BACKUP_DIR } from './constants';
 
 interface WrapperOptions {
   excluded?: boolean;
 }
+
+export interface ReadCommandTask {
+  command: string;
+  title: string;
+}
+
+export const readCommandOutputs = (...commands: string[]): string[] =>
+  commands.map(command => execSync(command).toString());
+
+export const composeReadCommandTask = (prefix: string) => ({ command, title }: ReadCommandTask): Undertaker.Task => {
+  const output = readCommandOutputs(command).toString();
+  const data = _.compact(output.split('\n'));
+  const backupTask = (): NodeJS.ReadWriteStream =>
+    file(`${title}.json`, JSON.stringify(data), { src: true }).pipe(gulp.dest([BACKUP_DIR, prefix].join('/')));
+
+  // Rename sub tasks
+  Object.defineProperty(backupTask, 'name', { value: [prefix, _.capitalize(title)].join(''), configurable: true });
+
+  return backupTask;
+};
 
 export const wrapHomeDir = (filename: string, opts: WrapperOptions = {}): string => {
   const { excluded = false } = opts;
