718dde9c3456b3db28b3893c42b32a637a2fc429
Ethan <thanhnx.605@gmail.com>
2020-04-07 16:47:47 +0700

    :sparkles: Add casks & taps for brew backup

--- gulpfile.ts/backup/brew.ts
+++ gulpfile.ts/backup/brew.ts
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import Undertaker from 'undertaker';
 
 // Gulp modules
 import gulp from 'gulp';
@@ -10,10 +11,28 @@ import { readCommandOutputs } from '../helpers';
 // Constants
 import { BACKUP_DIR } from '../constants';
 
-const brew = (): NodeJS.ReadWriteStream => {
-  const output = readCommandOutputs('brew list').toString();
-  const packages = _.compact(output.split('\n'));
-  return file('packages.json', JSON.stringify(packages), { src: true }).pipe(gulp.dest(`${BACKUP_DIR}/brew`));
+interface Task {
+  command: string;
+  title: string;
+}
+
+const TASKS: Task[] = [
+  { command: 'brew list', title: 'formulaes' },
+  { command: 'brew cask list', title: 'casks' },
+  { command: 'brew tap', title: 'taps' },
+];
+
+const composeBackupTask = ({ command, title }: Task): Undertaker.Task => {
+  const output = readCommandOutputs(command).toString();
+  const data = _.compact(output.split('\n'));
+  const backupTask = (): NodeJS.ReadWriteStream =>
+    file(`${title}.json`, JSON.stringify(data), { src: true }).pipe(gulp.dest(`${BACKUP_DIR}/brew`));
+
+  // Rename sub tasks
+  Object.defineProperty(backupTask, 'name', { value: `backupBrew${_.capitalize(title)}`, configurable: true });
+
+  return backupTask;
 };
 
-export default brew;
+gulp.task('brew', gulp.parallel(TASKS.map(composeBackupTask)));
+export default gulp.parallel(TASKS.map(composeBackupTask));
