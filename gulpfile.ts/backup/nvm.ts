98c2da5f94de74cc901aec70f4e2911692bdf1e3
Ethan <thanhnx.605@gmail.com>
2020-04-07 20:15:46 +0700

    :sparkles: Add global npm packages for nvm

--- gulpfile.ts/backup/nvm.ts
+++ gulpfile.ts/backup/nvm.ts
@@ -1,19 +1,16 @@
-import _ from 'lodash';
-
 // Gulp modules
 import gulp from 'gulp';
-import file from 'gulp-file';
 
 // Helpers
-import { readCommandOutputs } from '../helpers';
+import { composeReadCommandTask } from '../helpers';
+import type { ReadCommandTask } from '../helpers';
 
-// Constants
-import { BACKUP_DIR } from '../constants';
+const TASKS: ReadCommandTask[] = [
+  { title: 'nodeVersions', command: 'source $NVM_DIR/nvm.sh && nvm_ls' },
+  {
+    title: 'globalNpmPackages',
+    command: 'ls $(npm config get prefix)/lib/node_modules',
+  },
+];
 
-const nvm = (): NodeJS.ReadWriteStream => {
-  const output = readCommandOutputs('source $NVM_DIR/nvm.sh && nvm_ls').toString();
-  const versions = _.compact(output.split('\n'));
-  return file('versions.json', JSON.stringify(versions), { src: true }).pipe(gulp.dest(`${BACKUP_DIR}/nvm`));
-};
-
-export default nvm;
+export default gulp.parallel(TASKS.map(composeReadCommandTask('nvm')));
