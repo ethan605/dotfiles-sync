e7ddae9cc1bbb523fe55b634fb5a8d127f00e738
Ethan <thanhnx.605@gmail.com>
2020-04-07 16:17:14 +0700

    :sparkles: Use JSON instead of plain text for brew backup

--- gulpfile.ts/backup/brew.ts
+++ gulpfile.ts/backup/brew.ts
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 // Gulp modules
 import gulp from 'gulp';
 import file from 'gulp-file';
@@ -9,8 +11,10 @@ import { readCommandOutputs } from '../helpers';
 import { BACKUP_DIR } from '../constants';
 
 const brew = (): NodeJS.ReadWriteStream => {
-  const packages = readCommandOutputs('brew list --versions').toString();
-  return file('packages', packages, { src: true }).pipe(gulp.dest(`${BACKUP_DIR}/brew`));
+  const output = readCommandOutputs('brew list').toString();
+  const packages = _.compact(output.split('\n'));
+  return file('packages.json', JSON.stringify(packages), { src: true }).pipe(gulp.dest(`${BACKUP_DIR}/brew`));
 };
 
+gulp.task('brew', brew);
 export default brew;
