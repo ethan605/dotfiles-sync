052b7c7b3bdff8473043bc53bb54cab55ecf8611
Ethan <thanhnx.605@gmail.com>
2020-04-07 21:35:37 +0700

    :sparkles: Use /bin/zsh to run scripts

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -2,5 +2,8 @@
 source scripts/helpers.sh
 
 install_prerequisites() {
-
+  print_step "Install prerequisites"
+  # /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 }
+
+install_prerequisites
