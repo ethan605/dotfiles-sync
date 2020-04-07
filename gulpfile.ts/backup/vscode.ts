7a740ec3375def0b84500cae931b4852d9da46fb
Ethan <thanhnx.605@gmail.com>
2020-04-07 16:22:35 +0700

    :sparkles: Use JSON instead of plain text for vscode backup

--- gulpfile.ts/backup/vscode.ts
+++ gulpfile.ts/backup/vscode.ts
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 // Gulp modules
 import gulp from 'gulp';
 import file from 'gulp-file';
@@ -12,13 +14,15 @@ const VSCODE_COMMAND = '"/Applications/Visual Studio Code.app/Contents/Resources
 const VSCODE_SETTINGS_PATH = 'Library/Application Support/Code/User/settings.json';
 
 const vscode = (): NodeJS.ReadWriteStream => {
-  const extensions = readCommandOutputs(`${VSCODE_COMMAND} --list-extensions`).toString();
+  const output = readCommandOutputs(`${VSCODE_COMMAND} --list-extensions`).toString();
+  const extensions = _.compact(output.split('\n'));
   const settings = wrapHomeDir(VSCODE_SETTINGS_PATH);
 
   return gulp
     .src(settings)
-    .pipe(file('extensions', extensions))
+    .pipe(file('extensions.json', JSON.stringify(extensions)))
     .pipe(gulp.dest(`${BACKUP_DIR}/vscode`));
 };
 
+gulp.task('vscode', vscode);
 export default vscode;
