c63f4234526cdbfe1a03e7261119f63d5b5f99ec
Ethan Nguyen <thanhnx.605@gmail.com>
2020-04-09 22:04:22 +0700

    :recycles: Refactor backup script to work with launchctl job

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,7 +2,7 @@
 WORK_DIR=$HOME/Documents/Workspaces/Personal/dotfiles
 PATH=/usr/local/bin:$WORK_DIR/node_modules/.bin:$PATH
 ZSH=$HOME/.oh-my-zsh
-# unset PREFIX
+unset PREFIX
 
 source $WORK_DIR/scripts/helpers.sh
 source /usr/local/opt/nvm/nvm.sh
@@ -35,9 +35,9 @@ commit_and_push() {
 print_timestamp "Start backup"
 
 cd $WORK_DIR
-update_system_sources
-run_gulp_tasks
-commit_and_push
+update_system_sources && \
+run_gulp_tasks && \
+commit_and_push && \
 independent_update_tasks
 
 print_timestamp "Finish backup"
