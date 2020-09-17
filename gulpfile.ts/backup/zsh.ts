09b512953a86b227fb0c7521162928f81fbd9ac0
Ethan Nguyen <thanhnx.605@gmail.com>
2020-09-17 12:23:43 +0100

    Remove unused secrets in .zshrc

--- gulpfile.ts/backup/zsh.ts
+++ gulpfile.ts/backup/zsh.ts
@@ -13,11 +13,6 @@ const DEST_DIR = `${BACKUP_DIR}/zsh`;
 const zshrc = (): NodeJS.ReadWriteStream =>
   gulp
     .src(wrapHomeDir('.zshrc'))
-    .pipe(replace(/HOMEBREW_GITHUB_API_TOKEN=".*"/, 'HOMEBREW_GITHUB_API_TOKEN=""'))
-    .pipe(replace(/JEKYLL_GITHUB_TOKEN=".*"/, 'JEKYLL_GITHUB_TOKEN=""'))
-    .pipe(replace(/PITOP_GITHUB_TOKEN=".*"/, 'PITOP_GITHUB_TOKEN=""'))
-    .pipe(replace(/GOOGLE_APPLICATION_CREDENTIALS=".*"/, 'GOOGLE_APPLICATION_CREDENTIALS=""'))
-    .pipe(replace(/GOOGLE_PROJECT_ID=".*"/, 'GOOGLE_PROJECT_ID=""'))
     .pipe(replace(/SDK_TOKEN_FACTORY_SECRET=".*"/, 'SDK_TOKEN_FACTORY_SECRET=""'))
     .pipe(replace(/LOKALISE_TOKEN=".*"/, 'LOKALISE_TOKEN=""'))
     .pipe(replace(/LOKALISE_PROJECT_ID=".*"/, 'LOKALISE_PROJECT_ID=""'))
